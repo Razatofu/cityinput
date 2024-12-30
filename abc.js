333a5de5234d366bf552c1d0e52556f93fe52a71
raza ayaz <razaayaz@yahoo.com>
2024-12-31 00:13:24 +0500

    Add files via upload

---
+++ abc.js
@@ -0,0 +1,13 @@
+// var cityName = prompt ('enter your city','city name');
+var question = "Your city";
+ var defaultAnswer = "karachi";
+ var spec = prompt(question, defaultAnswer);
+
+
+
+if(spec == "karachi"  ){
+     document.write('WELCOME TO THE CITY OF LIGHT')
+}
+else{
+    document.write("you are not a citizen of karachi")
+}
